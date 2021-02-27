ca011d0ef17acb8daf24efc6649d5685e5a62b5d
DavidUps <darribas_talentomobile@bankinter.com>
2021-02-27 22:16:09 +0100

    finish navigation

---
+++ app/src/main/java/com/davidups/puppy_adoption/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.puppyadoption.core
+
+class Constants {
+
+    companion object {
+        const val POPPY_LIST = "poppy list"
+        const val POPPY_DETAIL = "poppy detail"
+    }
+}
